c3e07649e2411db090a62f6440a4e2c9de557e26
markcecon <mark.cecon@gmail.com>
2023-11-09 19:25:57 -0300

    Add name property to FeatureEntity and IFeature interfaces

--- src/feature/entities/feature.entity.ts
+++ src/feature/entities/feature.entity.ts
@@ -2,32 +2,33 @@ import { EAppMode } from '../../app';
 import { IFeature } from '../interfaces';
 
 export class FeatureEntity implements IFeature {
-    // #region Properties (10)
+  // #region Properties (11)
 
-    public appId: string = '';
-    public appMode: EAppMode | null = null;
-    public companyId: string = '';
-    public containerId: string = '';
-    public createdAt: Date = new Date();
-    public deviceId: string = '';
-    public id: string = '';
-    public installationId: string = '';
-    public planId: string | null = null;
-    public subscriptionId: string = '';
+  public appId: string = '';
+  public appMode: EAppMode | null = null;
+  public companyId: string = '';
+  public containerId: string = '';
+  public createdAt: Date = new Date();
+  public deviceId: string = '';
+  public id: string = '';
+  public installationId: string = '';
+  public name: string = '';
+  public planId: string | null = null;
+  public subscriptionId: string = '';
 
-    // #endregion Properties (10)
+  // #endregion Properties (11)
 
-    // #region Constructors (1)
+  // #region Constructors (1)
 
-    constructor(data?: Partial<FeatureEntity>) {
-        if (data) {
-            for (let key in data) {
-                if (data.hasOwnProperty(key) && key in this) {
-                    (this as any)[key] = (data as any)[key];
-                }
-            }
+  constructor(data?: Partial<FeatureEntity>) {
+    if (data) {
+      for (let key in data) {
+        if (data.hasOwnProperty(key) && key in this) {
+          (this as any)[key] = (data as any)[key];
         }
+      }
     }
+  }
 
-    // #endregion Constructors (1)
+  // #endregion Constructors (1)
 }
