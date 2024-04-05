1373e7f8915437025084da30ee51bf2f009ad658
Mark Cecon <mark.cecon@gmail.com>
2024-04-05 09:44:30 -0300

    Add new files and update versions

---
+++ src/fcm-token-message/interfaces/index.ts
@@ -0,0 +1,3 @@
+export { IFcmDataReceivedDesenfilaPayment } from './i-fcm-data-received-desenfila-payment';
+export { IFcmDataRequestItems } from './i-fcm-data-request-items';
+export { IFcmTokenMessage } from './i-token';
