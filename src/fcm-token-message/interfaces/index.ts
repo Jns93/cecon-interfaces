9c43e8e773ed4ff086b030cf54c0d145f96dc87b
Jonas Pereira <jonaspplc@hotmail.com>
2024-04-10 16:47:57 -0300

    create interface to default fcm notify

--- src/fcm-token-message/interfaces/index.ts
+++ src/fcm-token-message/interfaces/index.ts
@@ -1,3 +1,4 @@
 export { IFcmDataReceivedDesenfilaPayment } from './i-fcm-data-received-desenfila-payment';
 export { IFcmDataRequestItems } from './i-fcm-data-request-items';
 export { IFcmTokenMessage } from './i-token';
+import { IFcmNotify } from './i-fcm-norify';
